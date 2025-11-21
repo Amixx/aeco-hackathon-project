df8ba66415a00dc826f526a0b32432ca5e70e774
Imants Liepiņš <iliepins01@gmail.com>
2025-11-21 17:44:19 +0100

    Add github pages workflow.

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [svelte(), tailwindcss()],
-});
+  base: command === "build" ? "/aeco-hackathon-project/" : "/",
+}));
