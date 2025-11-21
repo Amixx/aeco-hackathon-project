5eb9785797f7246c38e50ff4dc761daf12218601
Imants Liepiņš <iliepins01@gmail.com>
2025-11-21 15:12:07 +0100

    Add eslint, some shared settings.

---
+++ eslint.config.ts
@@ -0,0 +1,35 @@
+import js from "@eslint/js";
+import svelte from "eslint-plugin-svelte";
+import globals from "globals";
+import ts from "typescript-eslint";
+
+export default ts.config(
+  js.configs.recommended,
+  ...ts.configs.recommended,
+  ...svelte.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+  {
+    files: ["**/*.svelte", "**/*.svelte.ts", "**/*.svelte.js"],
+    // See more details at: https://typescript-eslint.io/packages/parser/
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        extraFileExtensions: [".svelte"], // Add support for additional file extensions, such as .svelte
+        parser: ts.parser,
+      },
+    },
+  },
+  {
+    rules: {
+      // Override or add rule settings here, such as:
+      // 'svelte/rule-name': 'error'
+    },
+  }
+);
