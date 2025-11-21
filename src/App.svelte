0b3e7fa671bf7c89e65f0d05350968675fbb4270
Imants Liepiņš <iliepins01@gmail.com>
2025-11-21 14:55:03 +0100

    Add tailwind & flowbite.

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
-  import Counter from "./lib/Counter.svelte";
+  import { Alert } from "flowbite-svelte";
 </script>
 
-<main>
-  <div class="card">
-    <Counter />
-  </div>
-</main>
+<div class="p-8">
+  <Alert>
+    <span class="font-medium">Info alert!</span>
+    Change a few things up and try submitting again.
+  </Alert>
+</div>
