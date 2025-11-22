7e06153036ff3b602ad38054c919ddbcd7353b42
Imants Liepiņš <iliepins01@gmail.com>
2025-11-22 15:18:45 +0100

    Restore hash based routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import { createRouter, RouterProvider } from "@tanstack/react-router";
+import { createHashHistory, createRouter, RouterProvider } from "@tanstack/react-router";
 
 import { routeTree } from "./routeTree.gen";
 
+const hashHistory = createHashHistory();
+
 const router = createRouter({
 	routeTree,
+	history: hashHistory,
 });
 
 declare module "@tanstack/react-router" {
